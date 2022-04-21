5e19ac3a19356bef1a30c5afd364bb4dc948b4b8
murodalidev <murodalinarzullaevofficial@gmail.com>
2022-04-21 12:25:39 +0500

    reset password

--- core/urls.py
+++ core/urls.py
@@ -15,6 +15,9 @@ urlpatterns = [
     path('reset-confirm/<uidb64>/<token>/', auth_view.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset-complete/', auth_view.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
+    path('change-password/', auth_view.PasswordChangeView.as_view(template_name='account/change_password.html'), name='change_password'),
+    path('change-password-done/', auth_view.PasswordChangeDoneView.as_view(template_name='account/change_password_done.html'), name='password_change_done'),
+
 ]
 
 """
